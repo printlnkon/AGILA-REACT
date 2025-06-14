f05bb6850420650bbd1f651184084ecd46a0c621
Kalfontein Cruz <cruz.310690@caloocan.sti.edu.ph>
2025-06-15 02:46:00 +0800

    admin dashboard
    - added admin dashboard
    - added error message
    - added 404 error
    - accounts, departments, schedules are under development

---
+++ frontend/src/components/AdminDashboard/Schedules.jsx
@@ -0,0 +1,11 @@
+export default function Schedules() {
+  return (
+    <>
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="text-2xl font-bold text-blue-900">
+          Under Construction
+        </div>
+      </div>
+    </>
+  );
+}
