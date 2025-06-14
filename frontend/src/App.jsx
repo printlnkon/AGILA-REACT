7ec476f14306f923ca0131d6eea36e64f00c804b
Kalfontein Cruz <cruz.310690@caloocan.sti.edu.ph>
2025-06-15 02:46:00 +0800

    admin dashboard
    - added admin dashboard
    - added error message
    - added 404 error
    - accounts, departments, schedules are under development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import ErrorMessage from "@/components/ErrorMessage";
+import PageNotFound from "@/components/PageNotFound";
 import Index from "@/pages/Index";
 import Login from "@/pages/Login";
 import Admin from "@/pages/Admin";
+import Dashboard from "@/components/AdminDashboard/DashBoard";
+import Accounts from "@/components/AdminDashboard/Accounts";
+import Departments from "@/components/AdminDashboard/Departments";
+import Schedules from "@/components/AdminDashboard/Schedules";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
+      <ErrorMessage>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />}>
+              <Route index element={<Dashboard />} />
+              <Route path="accounts" element={<Accounts />} />
+              <Route path="departments" element={<Departments />} />
+              <Route path="schedules" element={<Schedules />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
+      </ErrorMessage>
     </>
   );
 }
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,25 @@
+import ErrorMessage from "@/components/ErrorMessage";
 import SideBar from "@/components/AdminDashboard/SideBar";
+import Header from "@/components/AdminDashboard/Header";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { Outlet } from "react-router-dom";
 
 export default function Admin() {
   return (
     <>
-      <SideBar />
+      <SidebarProvider>
+        {/* sidebar */}
+        <ErrorMessage>
+          <SideBar />
+        </ErrorMessage>
+
+        <SidebarInset>
+          {/* breadcrumbs */}
+          <Header />
+          {/* main */}
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
     </>
   );
 }
